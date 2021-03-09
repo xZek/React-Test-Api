bfc9417e958f154d36a31555dc5365bd6b32ab25
Zekeriya Uysal <zekeriyauysal12@gmail.com>
2021-03-09 15:26:06 +0300

    Initial commit

---
+++ src/components/PageNotFound.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+
+
+const PageNotFount = () =>{
+
+    return(
+
+        <p>
+         Page Not Found. Go To <Link to="/">Home</Link>   
+        </p>
+
+    );
+
+
+}
+
+export default PageNotFount
