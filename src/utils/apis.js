8a54ecfc09f084cee83ee07e32dce07211b07956
Zekeriya Uysal <zekeriyauysal12@gmail.com>
2021-03-09 15:38:42 +0300

    test

--- src/utils/apis.js
+++ src/utils/apis.js
@@ -3,7 +3,7 @@ import { BASE_API_URL } from './constants';
 
 const getStory = async (id) => {
   try {
-    const story = await axios.get(`${BASE_API_URL}/item/${id}.json`);
+    const story = await axios.get('${BASE_API_URL}/item/${id}.json');
     return story;
   } catch (error) {
     console.log('Error while getting a story.');
